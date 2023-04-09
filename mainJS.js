33f0751ac442052b410572411110ea84f8eafc2c
plantbasedcode <33013566+plantbasedcode@users.noreply.github.com>
2023-04-10 00:52:38 +0200

    ADDED TOGGLE DIV BUTTON
    HTML
    commented out old gallery
    pasted side media scroller into dropDownGallery div
    turned Art Gallery 2.0 into a external JS button
    
    CSS
    i think i undid all my edits but adding a space
    
    JS
    added my function get element by ID - w3 school modified toggled hide show div
    added window onload function to hide gallery on page load so you have to press the buttonfor it to ´´drop down´´

--- mainJS.js
+++ mainJS.js
@@ -1,1 +1,14 @@
-﻿
+﻿window.onload = function(){
+    document.getElementById("dropDownGallery").style.display='none';
+  };
+//this hides the scroller on load
+
+function myFunction() {
+    var x = document.getElementById("dropDownGallery");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  // actual button js from W3
